b0c1288061088b524cc2eb51fa3c359b6c4a8ff9
Gustavo Henrique <gustavohpereira1.6@gmail.com>
2024-02-08 17:57:55 -0300

    finish POST /polls/:pollId/votes route

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -15,7 +15,52 @@ export async function voteOnPoll(app: FastifyInstance) {
     const { pollOptionId } = voteOnPollBody.parse(request.body)
     const { pollId } = voteOnPollParams.parse(request.params)
 
-    const sessionId = crypto.randomUUID()
+    let { sessionId } = request.cookies
+
+    if (sessionId) {
+      const userPreviousVoteOnPoll = await prisma.vote.findUnique({
+        where: {
+          sessionId_pollId: {
+            sessionId,
+            pollId,
+          },
+        },
+      })
+
+      if (
+        userPreviousVoteOnPoll &&
+        userPreviousVoteOnPoll.pollOptionId !== pollOptionId
+      ) {
+        await prisma.vote.delete({
+          where: {
+            id: userPreviousVoteOnPoll.id,
+          },
+        })
+      } else if (userPreviousVoteOnPoll) {
+        return reply
+          .status(400)
+          .send({ message: 'You already voted on this poll.' })
+      }
+    }
+
+    if (!sessionId) {
+      sessionId = crypto.randomUUID()
+
+      reply.setCookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 30, // 30 days,
+        signed: true,
+        httpOnly: true,
+      })
+    }
+
+    await prisma.vote.create({
+      data: {
+        sessionId,
+        pollId,
+        pollOptionId,
+      },
+    })
 
     return reply.status(201).send()
   })
